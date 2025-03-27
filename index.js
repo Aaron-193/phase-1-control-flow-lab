2f697d3dc255f31ab7effd72716c05d37991254e
Aaronrashid <anr.ron193@gmail.com>
2025-03-27 16:31:08 +0300

    Updates js file

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(diveDepth){
   // Write your code here!
+  if(diveDepth<=400) {
+    return 'This one is on me!';
+  } else if(diveDepth<2000 && diveDepth>400) {
+    return 'That will be twenty bucks.';
+  } else if(diveDepth>2000 && diveDepth<=2500) {
+    return'I will gladly take your thirty bucks.';
+  } else if(diveDepth>2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  if(cityName === 'NYC') {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
-
-function switchOnCharmFromTip(){
+scuberGreetingForFeet(199);
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+  if(tipAmount === 'generous') {
+    return "Thank you so much.";
+  } else if('not as generous') {
+    return "Thank you.";
+  } else {
+    return 'Bye.'
+  } 
 }
